968c43538dcb2455c659b58ee17361fbe875f960
shihezichen <shihezichen@live.cn>
2020-05-26 22:15:58 +0800

    gym_005 modify

--- gym/gym_005.py
+++ gym/gym_005.py
@@ -1,7 +1,7 @@
 import gym
 import numpy as np
 
-<<<<<<< HEAD
+
 # build the environment
 gEnv = gym.make("FrozenLake-v0")
 
@@ -64,26 +64,3 @@ optimal_value_function = value_iteration(env=gEnv, gamma=1.0)
 optimal_policy = extract_policy(env=gEnv, value_table=optimal_value_function, gamma=1.0)
 
 print(optimal_policy)
-=======
-
-# build the environment
-env = gym.make("FrozenLake-v0")
-
-print( "show env:")
-env.render()
-
-# show the enviroment size: 4*4=16
-print( "env space size: ", env.observation_space.n)
-
-# show the action space size: 上下左右
-print( "action space size: ", env.action_space.n)
-
-# initial the V(s) to zero
-value_table = np.zeros( env.observation_space.n )
-# iterate 100000 times
-no_of_iterations = 10**5
-
-for i in range( no_of_iterations ):
-    # 
-    updated_value_table = np.copy( value_table )
->>>>>>> master
