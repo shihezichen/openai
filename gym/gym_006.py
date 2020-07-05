bc770fd7c6a62a9279529fc5f8d62f7808594f48
shihezichen <shihezichen@live.cn>
2020-07-05 10:37:21 +0800

    MentoCarlo
    add example of  Monte Carlo

--- gym/gym_006.py
+++ gym/gym_006.py
@@ -1,34 +1,51 @@
-import tensorflow as tf
-import cv2
-import sys
-import random
 import numpy as np
-from collections import deque
-
-#sys.path('game/')
-
-GAME = 'bird'
-ACTIONS = 2
-OBSERVE = 10**4
-EXPLORE = 2*(10**5)
-FINAL_EPSILON = 10**(-4)
-BATCH = 32
-
-print(FINAL_EPSILON)
-print(cv2.__version__)
-
-def createNetwork():
-    pass
-
-def playGame():
-    sess = tf.InteractiveSession()
-    s, readout, h_fcl = createNetwork()
-
-def main():
-    playGame()
-
-if __name__ == 'main':
-    main()
+import math
+import random
+import matplotlib.pyplot as plt
 
 
+# 随机产生点x,y, 取值均在范围 [0, size]
+def generatePts(size):
+    x = random.random() * size
+    y = random.random() * size
+    return x, y
 
+
+# 是否在圆内
+def isInCircle(pt, r):
+    # 到圆心的距离小于半径
+    return math.sqrt(pt[0] ** 2 + pt[1] ** 2) <= r
+
+# 画出正方形和1/4圆弧
+def draw():
+    plt.axes().set_aspect('equal')
+    arc = np.linspace(0, np.pi / 2, 100)
+    plt.plot(1 * np.cos(arc), 1 * np.sin(arc))
+
+# 随机数方式计算pi
+def calcPi():
+    # 边长
+    a = 1
+    # 落入园弧的点的个数
+    ptsInCircle = 0
+    # 落入正方形的店的个数
+    ptsInSquare = 0
+    # 样本数量
+    samples = 1000
+
+    draw()
+    for i in range(samples):
+        pt = generatePts(a)
+        plt.plot(pt[0], pt[1], 'c.')
+        ptsInSquare += 1
+        if isInCircle(pt, a):
+            ptsInCircle += 1
+
+    plt.show()
+    # 计算Pi
+    return 4 * ptsInCircle / ptsInSquare
+
+
+if __name__ == '__main__':
+    pi = calcPi()
+    print('PI is: {} '.format(pi))
