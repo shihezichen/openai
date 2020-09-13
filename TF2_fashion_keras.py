89e3ec17784f2d483f916d9c230e9e7108dd6b3a
边城量子 <shihezichen@live.cn>
2020-09-13 14:21:52 +0800

    LeNet, AlexNet, VGG, etc

--- TF2_fashion_keras.py
+++ TF2_fashion_keras.py
@@ -17,4 +17,26 @@ model.compile(optimizer='adam',
 model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
 model.summary()
 
-
+# 预测
+#img_path = input("Path of test picture:")
+BASE_DIR='/home/arthur/Study/AI_Pic/MyHandwritePics/'
+img = Image.open(BASE_DIR + "test.png")
+img = img.resize((28, 28), Image.ANTIALIAS)
+img_arr = np.array(img.convert('L'))
+# 方法1:  取反, 白底黑字 变为 黑底白字, 仍然是灰度
+img_arr = 255 - img_arr
+# 方法2: 变为只有黑白色的高对比图片
+# for i in range(28):
+#     for j in range(28):
+#         if img_arr[i][j] < 20-:
+#             img_arr[i][j] = 255
+#         else:
+#             img_arr[i][j] = 0
+# 归一化
+img_arr = img_arr / 255.0
+# 训练时都是batch输入的, 因此要把 (28, 28) 前添加一个维度,变为 (1, 28, 28)
+x_predict = img_arr[tf.newaxis, ...]
+result = model.predict(x_predict)
+pred = tf.argmax(result, axis=1)
+print('\n')
+tf.print(pred)
